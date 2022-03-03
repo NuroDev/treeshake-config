b4744442a0c9661f162855d7e4be83abbcb474ed
nurodev <me@nuro.dev>
2022-03-03 10:46:58 +0000

    Removed `config` types
    Removing these for now until config handling is added

--- src/types/config.ts
+++
@@ -1,8 +0,0 @@
-export interface Config {
-  /**
-   * Path to the root config file
-   *
-   * @default `config.js` or `config.ts` in the same directory as the project `package.json`
-   */
-  root: string;
-}
